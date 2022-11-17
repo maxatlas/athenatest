9f388101850744c78b4d81ed0a469903740725dd
maxine <maxine.lyu@gmail.com>
2022-11-17 16:58:45 +1000

    metrics.py reformat

--- metrics.py
+++ metrics.py
@@ -1,44 +1,25 @@
+import torch
+import matplotlib.pyplot as plt
 import config as c
-from pandas import DataFrame
-
 from pathlib import Path
 
 
-def get_mce(y_pred, y_true, benchmark_session_id="") -> DataFrame:
-    if not y_pred:
-        return DataFrame(["Bad session: Deviated more than %f from session %s results" %
-                          (c.deviation_threshold, benchmark_session_id)])
-    mce = DataFrame([])
+def get_mce(y_conf: torch.Tensor, y_true: torch.Tensor, benchmark_session_id: str = ""):
+    mce = 0
     return mce
 
 
-def get_ece(y_pred, y_true, benchmark_session_id="") -> DataFrame:
-    if not y_pred:
-        return DataFrame(["Bad session: Deviated more than %f from session %s results" %
-                          (c.deviation_threshold, benchmark_session_id)])
-    ece =  DataFrame([])
+def get_ece(y_conf: torch.Tensor, y_true: torch.Tensor, benchmark_session_id: str = ""):
+    ece = 0
+
     return ece
 
 
-def get_confusion_matrix(y_pred, y_true, benchmark_session_id="") -> DataFrame:
-    if not y_pred:
-        return DataFrame(["Bad session: Deviated more than %f from session %s results" %
-                          (c.deviation_threshold, benchmark_session_id)])
-    cm = DataFrame([])
-    return cm
-
-
-def plot_mce(df: DataFrame):
+def plot_mce(df: torch.Tensor, save_path: str):
     # save fig in folder
     return
 
 
-def plot_ece(df: DataFrame):
+def plot_ece(df: torch.Tensor, save_path: str):
     # save fig in folder
     return
-
-
-def plot_confusion_matrix(df: DataFrame):
-    # save fig in folder
-    return
-
