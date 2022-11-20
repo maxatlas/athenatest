bf892ee498b9ed667cc29c3bf13e8e27c312576e
maxine <maxine.lyu@gmail.com>
2022-11-20 10:21:51 +1000

    remove outdated config values.

--- config.py
+++ config.py
@@ -3,27 +3,19 @@ Define hyper-parameters here.
 
 """
 
-delimiter = "+"
-min_n = 20
-
 accuracy_threshold = 0
 
 res_folder_path = "../results"
 fp_folder_path = "%s/false_positives" % res_folder_path
 
-mce_table_name = "MCE"
-ece_table_name = "ECE"
-cm_table_name = "confusion_matrix"
-
 batch_size_test = 100
-
 random_seed = 122
 
 k = 10
+
 data_folder_path = "../data/cifar10/test"
 test_output_folder = "vis/"
 shuffle_dataloader = True
 
 pad = 2
-decimal_precision = 5
 
