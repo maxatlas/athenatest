2dadc4544f409cce2779540131595cc200b38329
maxine <maxine.lyu@gmail.com>
2022-11-21 13:51:26 +1000

    better param explanation

--- config.py
+++ config.py
@@ -4,24 +4,21 @@ Define hyper-parameters here.
 """
 
 accuracy_threshold = 0
+model = "tiny"
+data_folder_path = "../data/cifar10/test"
+device = "cuda:0"
+save_fp = False
+
 
 res_folder_path = "results"
 test_fp_folder = "testing/results/false_positives"
 fp_folder_path = "%s/false_positives" % res_folder_path
-
-batch_size_test = 100
-random_seed = 122
-
-k = 10
-
-model = "tiny"
-data_folder_path = "../data/cifar10/test"
 test_plot_folder = "vis/"
+
+batch_size_test = 100  # batch size for test set
+random_seed = 122
+k = 10  # Bin size
 shuffle_dataloader = True
 
-pad = 2
-
-device = "cuda:0"
-save_fp = False
-
-testing_limit = 3  # Use 3 samples for testing
+pad = 2  # image padding. pad < 2 will break MNIST eval
+testing_limit = 3  # Use 3 samples for fp save test
